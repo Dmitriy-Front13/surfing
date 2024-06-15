b9e3af5432cf4e3b3220819b49a4a3ef853b7e1e
Dmytro Klymenko <dimaklim04@gmail.com>
2024-06-15 19:59:28 +0300

    vse krome title i color

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,33 @@ $(function () {
   $('.slider__items').slick({
     arrows: false,
     dots: true,
-    });
-    
-    $('.menu__btn').on('click', function(){
-      $('.menu__list').toggleClass('menu__list--active');
-      $('.menu__btn').toggleClass('menu__btn--active');
-      });
-    var mixer = mixitup('.gallery__content');
+  });
+
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active');
+    $('.menu__btn').toggleClass('menu__btn--active');
+  });
+
+  $('.mobile__btn').on('click', function () {
+    $('.mobile').fadeIn();
+    $(this).fadeOut();
+  });
+
+  const $svgElement = $('.icon__svg');
+  const header = $('.header__top')
+
+
+  $(window).on('scroll', function () {
+    const scrollY = $(window).scrollTop();
+
+    if (scrollY > 50) {
+      $svgElement.css('fill', '#00B2A0');
+      header.css('background-color', '#00B2A0');
+       
+    } else {
+      $svgElement.css('fill', '#fff');
+      header.css('background-color', 'transparent');
+    }
+  }); 
+  var mixer = mixitup('.gallery__content');
 });
